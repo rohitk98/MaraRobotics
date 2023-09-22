640332d08d4745a57e91d586e1a7a9dd0bd2e0c0
rohitk98 <87232311+rohitk98@users.noreply.github.com>
2023-09-22 05:17:03 +0000

    added dynamic content

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+Robots = [{
+    'id': 1,
+    'title': 'Fire Extingusher',
+    'price': '₹1000'
+}, {
+    'id': 2,
+    'title': 'Room Cleaner',
+    'price': '₹2000'
+}, {
+    'id': 3,
+    'title': 'Weed Plucker',
+    'price': '₹500'
+}]
 
 
 @app.route("/")
 def mara():
-  return render_template('home.html')
+  return render_template('home.html', robots=Robots)
+
+
+@app.route("/api/robots")
+def list_robots():
+  return jsonify(Robots)
 
 
 # print(__name__)
